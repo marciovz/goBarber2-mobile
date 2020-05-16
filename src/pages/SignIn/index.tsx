3354d5b27509e577ae3d0cc5a6e61cc6019c008c
marciovz <marciovz@yahoo.com.br>
2020-05-16 09:00:16 -0300

    Configurando importação de imagens png

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container, Text } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
+      <Image source={logoImg} />
       <Text>SIGNIN</Text>
     </Container>
   );
