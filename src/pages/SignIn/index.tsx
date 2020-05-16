23414f207e98926a0760c2607d14b988692f9e65
marciovz <marciovz@yahoo.com.br>
2020-05-16 09:41:15 -0300

    Importação e configuração de fontes externas

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Text } from './styles';
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,7 +9,7 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
-      <Text>SIGNIN</Text>
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
